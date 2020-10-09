896c5f580e726393210dd30051928dff808f25c3
Dominick Licciardi <dominick.nola@gmail.com>
2020-10-08 18:27:30 -0600

    working well, still a little slow.

--- src/PalindromesSearch.scala
+++ src/PalindromesSearch.scala
@@ -1,133 +1,103 @@
-import java.util.Scanner
 import java.util._
 import scala.collection.convert.ImplicitConversions.`list asScalaBuffer`
-import scala.collection.mutable
-import java.io._
 import java.io.FileWriter
 
 object PalindromesSearch {
-  var count = 0
   var target_count = 0
   var third_input = ""
   val filename = "OUTPUT_FILE_NAME.txt"
   val fw = new FileWriter(filename, true) //the true will append the new data
+  var seeResults = ""
+  var targetInt = 0
 
-  def process_list(mainList: ArrayList[ArrayList[Integer]], x: Int, y: Int,
-                   currentList: ArrayList[Integer]): Unit = {
-    if (y == 0) {
-      mainList.add(currentList)
-      return
-    } else if (x > y) {
-      return
+  // process the sum number(n) and the target number(m)
+  def process_list(sumNum: Int, targetNum: Int, currentCombo: ArrayList[Integer]): Unit = {
+    if (targetNum == 0 && currentCombo.contains(targetInt)) {
+        // gets the combinations of n from the below else/while loop when target number == 0 and the combo contains m,
+        // it then produces the permutations of each combination
+        currentCombo.permutations.foreach(eachPerm => {
+          // tests for palindrome and prints each one to an file called OUTPUT_FILE_NAME.txt if "y" is informed.
+          if (eachPerm == eachPerm.reverse) {
+            var i = 0
+            while (i < eachPerm.length) {
+              if (i < eachPerm.length - 1) {
+//                print(eachPerm(i) + ", ")
+                if (seeResults == "y")
+                  fw.write(eachPerm(i) + ", ") //appends the string to the file
+              } else {
+//                print(eachPerm(i))
+                if (seeResults == "y")
+                  fw.write(eachPerm(i) + " ")
+              }
+              i += 1
+            }
+//            println()
+            fw.write("\n")
+            // keeps count of found palindromic sequences
+            target_count += 1
+          }
+        })
+    } else if (sumNum > targetNum) {
     } else {
-      var j = x
-      while (j <= y) {
-        val newList = new ArrayList[Integer](currentList)
-        newList.add(j)
-        process_list(mainList, j, y - j, newList)
-        j += 1
+      // finds the combinations of n recursively. When combinations of n are found, the permutations are found above.
+      var countDown = sumNum
+      while (countDown <= targetNum) {
+        val newList = new ArrayList[Integer](currentCombo)
+        newList.add(countDown)
+        process_list(countDown, targetNum - countDown, newList)
+        countDown += 1
       }
     }
-    println(mainList.last)
-    mainList.last.permutations.foreach(x => {
-      val perms = x
-      if(isPalindrome(perms) && perms.contains(2)){
-        if (third_input == "y") {
-          var i = 0
-          while (i < perms.length) {
-            if (i < perms.length - 1) {
-              print(perms(i) + ", ")
-//              fw.write(perms(i) + ", ") //appends the string to the file
-            } else {
-              print(perms(i))
-//              fw.write(perms(i) + " ")
-            }
-            i += 1
-          }
-          println()
-//          fw.write("\n")
-        }
-        target_count += 1
-      }
-    })
   }
 
-//  def permutation(list: ArrayList[Integer], second_input: Int): Unit = {
-//    val indexing: mutable.Buffer[Integer] = list.map(z => z)
-//    indexing.permutations.foreach(x => {
-//      val perms = x
-//      if(isPalindrome(perms) && perms.contains(second_input)){
-//        if (third_input == "y") {
-//          var i = 0
-//          while (i < perms.length) {
-//            if (i < perms.length - 1) {
-//              print(perms(i) + ", ")
-//              fw.write(perms(i) + ", ") //appends the string to the file
-//            } else {
-//              print(perms(i))
-//              fw.write(perms(i) + " ")
-//            }
-//            i += 1
-//          }
-//          println()
-//          fw.write("\n")
-//        }
-//      target_count += 1
-//      }
-//    })
-//  }:Unit
-
-  def isPalindrome(list: mutable.Buffer[Integer]):Boolean = { list == list.reverse }
-
   def main(args: Array[String]): Unit = {
-    // 1: Get Input
-    val scanner_obj: Scanner = new Scanner(System.in)
-      println("Welcome to the palindromic sequence project!")
-      println("Enter n(sum total) and m(target int) to see the results: ")
-    val nextInt_1 = scanner_obj.nextLine().toInt
-    if (nextInt_1.equals("")) {
-      println("Use: java PalindromesSearch n m [y]\nextLine_1" +
+    // 1: Get Input form Terminal command-line and give suggestions if empty
+    if (args.length <= 1 ) {
+      println("Use: java PalindromesSearch n m [y]\n" +
         "[y]: when informed, all palindromic sequences must be saved to a file")
       System.exit(1)
     }
-    val nextInt_2 = scanner_obj.nextLine().toInt
-    if (nextInt_2.equals("")) {
-      println("Use: java PalindromesSearch n m [y]\nextLine_1" +
-        "[y]: when informed, all palindromic sequences must be saved to a file")
-      System.exit(1)
+    val BigNumString = args(0)
+    val BigNumInt = BigNumString.toInt
+    if (args.length == 3) {
+      seeResults = args(2)
     }
-    third_input = scanner_obj.nextLine()
+    val BigNum2String = args(1)
+    targetInt = BigNum2String.toInt
 
-    // 2: Send empty arrays to the list processor to fill.
-    val SolutionsList = new ArrayList[ArrayList[Integer]]()
+//    val scanner_obj: Scanner = new Scanner(System.in)
+    println("Welcome to the palindromic sequence project!")
+    if (BigNumString == "") {
+      println("Use: java PalindromesSearch n m [y]\n" +
+        "[y]: when informed, all palindromic sequences must be saved to a file")
+      System.exit(1)
+    }
+
+    // 2: Send empty array to the list processor to fill with current solution.
     val CurrentSolution = new ArrayList[Integer]()
+    println("Parameter n = " + BigNumInt + "\nParameter m = " + targetInt)
+    if(seeResults == "y")
+      println( "Generating palindromic sequences.." )
     val time1 = System.nanoTime()
-    process_list(SolutionsList, 1, nextInt_1, CurrentSolution)
+    process_list(1, BigNumInt, CurrentSolution)
     val time2 = System.nanoTime()
+    if (seeResults == "y") {
+      println("Done!")
+//      fw.write("Done!\n")
+    }
     val time = (time2 - time1) / 1000000000
 
-    //println(SolutionsList)
-
-    println("Parameter n = " + nextInt_1 + ",\nParameter m = " + nextInt_2)
-    fw.write("\nParameter n = " + nextInt_1 + ",\nParameter m = " + nextInt_2 + "\n")
-    if(third_input == "y") println("Generating palindromic sequences..")
-
-
-    // 3: Get all the permutations from the unique combinations and filter using second input m.
-//    var k = 0
-//    while (k <= SolutionsList.length - 1) {
-//      permutation(SolutionsList.get(k), nextInt_2)
-//      k += 1
-//    }
-
-    if (third_input == "y")
-      println("Done!")
-      fw.write("Done!\n")
-
-    println("Number of targeted palindromic sequences found: " + target_count.toString)
-    fw.write("Number of targeted palindromic sequences found: " + target_count.toString + "\n")
+    // parameter and time output
+//    fw.write("\nParameter n = " + BigNumInt + ",\nParameter m = " + BigNum2Int + "\n")
+    println("Number of filtered palindromic sequences found: " + target_count.toString)
+//    fw.write("Number of targeted palindromic sequences found: " + target_count.toString + "\n")
     println("It took me " + time + "s")
-    fw.write("It took me " + time.toString + "s\n")
+//    fw.write("It took me " + time.toString + "s\n")
     fw.close()
+
+    // output times
+    // input 35 = 70 min(4240s) 9-7-20
+    // input 34 = 41 min(2496s) 9-7-20
+    // input 33 = 24 min(1457s) 9-7-20
   }
 }
